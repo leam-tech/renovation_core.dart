d49844354976b8cc3675acd7697e5e935e3f451c
Mirza Nabil Shuja <nabilshuja@gmail.com>
2021-03-14 12:19:32 +0400

    fix: Exporting Docfield Type

--- lib/meta.dart
+++ lib/meta.dart
@@ -2,3 +2,4 @@ export 'src/meta/frappe/frappe.meta.controller.dart';
 export 'src/meta/frappe/interfaces.dart';
 export 'src/meta/meta.controller.dart';
 export 'src/meta/frappe/doctype.dart';
+export 'src/meta/frappe/docfield.dart';
